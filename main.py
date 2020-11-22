d6c07a4fafd6fe15958c66f525e02a78f9fbdc5c
nassorh <nassorh.dev@gmail.com>
2020-11-22 15:16:11 +0000

    Fetch extension was added, function was removed

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_headers(soup):
-	header = soup.find_all("h2")
-	for x in header:
-		print(x.text)
-	
+def fetchExtension(header):
+    #Find the child tag
+    data = header.find_next_sibling()
+    #Search for all hyperlinks
+    extension = data.find_all("a")
+    #clean extension
+    cleanExtension = [header.text]
+    for x in extension:
+        cleanExtension.append(x.text.strip())
+    print(cleanExtension)
+    return cleanExtension
+
 if __name__ == "__main__":
-	URL = 'https://www.online-convert.com/file-type'
-	page = requests.get(URL)
-	soup = BeautifulSoup(page.content,"html.parser")
-	fetch_headers(soup)
+    URL = 'https://www.online-convert.com/file-type'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content,"html.parser")
+    header = soup.find_all("h2")[1:]
+    for x in range(len(header)):
+        fetchExtension(header[x])
+        
